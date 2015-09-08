805f7f53f3e0959d1f18e99c4d0896d16ffa0304
Irving Gonzalez <irvingemmanuelgg@gmail.com>
2015-09-07 21:57:53 -0500

    Pruebas

---
+++ Pruebas/Prueba.java
@@ -0,0 +1,19 @@
+public class Prueba{
+	
+	public static void main(String[] args){
+
+		System.out.println("Pruebas");
+
+	}
+
+}
+
+// Este es un comentario de una sola linea
+/*
+Este es un comenatrio
+de varias lineas
+*/
+
+/**
+Esto es para la documentación
+**/
